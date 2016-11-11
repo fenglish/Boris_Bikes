4b307c466f25ad968a0dff25a77234ab357fbe95
James <jamesdix@cwgsy.net>
2016-11-11 16:04:59 +0000

    Ex 20 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -27,7 +27,7 @@ class DockingStation
 private
 
 def full?
-  @bikes.size == DEFAULT_CAPACITY
+  @bikes.size == @capacity
 end
 
 def no_availability?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,48 +2,51 @@ require 'docking_station'
 
 describe DockingStation do
 
+  let(:bike) { double :bike }
+
   describe "#release_bike" do
     it { is_expected.to respond_to :release_bike }
-
-    it "releases a bike" do
-      bikes = subject.dock_bike(Bike.new)
-      expect(subject.release_bike).to be_an_instance_of(Bike)
-    end
 
     it "should not release a bike when there are no bikes" do
       expect{ subject.release_bike }.to raise_error("No bikes available!")
     end
 
     it "should not release any bikes when all bikes are broken" do
-      20.times{ subject.dock_bike(Bike.new, false)}
+      station = DockingStation.new(1)
       expect{ subject.release_bike }.to raise_error("No bikes available!")
     end
 
   end
 
   describe "#dock_bike" do
+
     it { is_expected.to respond_to :dock_bike }
 
     it "docks a returned bike" do
-      expect(subject.dock_bike(Bike.new)).to be_truthy
+      bike = double(:bike, :working? => true)
+      expect(subject.dock_bike(bike)).to be_truthy
     end
 
     it "shows docked bikes when asked" do
-      #require 'pry'; binding.pry
-      expect( 3.times { subject.dock_bike(Bike.new) } ).to eq subject.bikes.size
+      # require 'pry'; binding.pry
+      bike = double(:bike, :working? => true)
+      expect( 1.times { subject.dock_bike(bike) } ).to eq subject.bikes.size
     end
 
     it "should not allow for more bikes than default capacity to be docked" do
-      bikes = DockingStation::DEFAULT_CAPACITY.times{ subject.dock_bike(Bike.new)}
-      expect{ subject.dock_bike(Bike.new)}.to raise_error("Docking station is full")
+      bike = double(:bike, :working? => true)
+      station2 = DockingStation.new(0)
+      expect{ station2.dock_bike(bike)}.to raise_error("Docking station is full")
     end
 
     it "should accept up to default capacity of bikes in each docking station" do
-      expect( DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(Bike.new) } ).to be_truthy
+      bike = double(:bike, :working? => true)
+      expect( DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(bike) } ).to be_truthy
     end
 
     it "docking station should only accept a bike object" do
-      returning_bike = Bike.new
+      bike = double(:bike, :working? => true)
+      returning_bike = bike
       expect(subject.dock_bike(returning_bike)).to eq true
     end
 
